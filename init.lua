533fafeb577e43e0ad9acb578b3b97ef3030a088
weishenggao <527518190@qq.com>
2022-02-09 22:32:02 +0800

    全屏快捷键

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, 'R', function ()
    hs.reload() 
 end)
+
 hs.alert('config reload')
 
 -- 显示 hello world
@@ -63,4 +64,15 @@ hs.hotkey.bind({"cmd", "alt" }, "up", function()
     win:setFrame(f)
 end)
 
- 
+hs.hotkey.bind({"cmd", "alt" }, "return", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  
+  f.x = max.x 
+  f.y = max.y 
+  f.w = max.w 
+  f.h = max.h 
+  win:setFrame(f)
+end)
